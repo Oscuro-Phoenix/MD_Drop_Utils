113641d62d0a42c32ce5a4d6f11db0d9f2b93f50
Oscuro-Phoenix <shakulpathak.iitkgp@gmail.com>
2021-02-12 20:11:22 +0530

    cleanup Feb11

--- density_Lib.py
+++ density_Lib.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 @jit
-def get_rho(cell_size,xlim,ylim,zlim,x0,y0,z0,padding,dx,dz):
+def get_rho(cell_size,xlim,ylim,zlim,x0,y0,z0,dx,dz):
     
     p = cell_size/2
-    q = padding
+    q = 0
     Nx = int(1/dx)
     Nz = int(1/dz)
     #Populate Density Matrix for each cell
-    rho = np.zeros(((xlim[1]-xlim[0]+2*q)*Nx,(zlim[1]-zlim[0]+2*q)*Nz))
+    rho = np.zeros(((xlim[1]-xlim[0])*Nx,(zlim[1]-zlim[0])*Nz))
 
-    for x in range(-q*Nx,(xlim[1]-xlim[0]+q)*Nx):
-        for z in range(-q*Nz,(zlim[1]-zlim[0]+q)*Nz):
+    for x in range(0,(xlim[1]-xlim[0])*Nx):
+        for z in range(0,(zlim[1]-zlim[0])*Nz):
             y_temp = y0[(x0>x/Nx+xlim[0]-p) & (x0<x/Nx+xlim[0]+p) & (z0>z/Nz+zlim[0]-p) & (z0<z/Nz+zlim[0]+p)]
             if y_temp.size != 0 and y_temp.max() != y_temp.min():
-                rho[x+q*Nx][z+q*Nz] = y_temp.shape[0]/(y_temp.max()-y_temp.min())
+                rho[x][z] = y_temp.shape[0]/(y_temp.max()-y_temp.min())
     return rho
 
 
@@ -25,14 +25,14 @@ def plot_snapshot(X, Y, Z, timestep):
     plt.scatter(X[:,timestep],Z[:,timestep])
 
 
-def plot_rho(rho,padding,xlim,zlim,dx,dz):
-    q = padding
+def plot_rho(rho,xlim,zlim,dx,dz):
+    q = 0
     Nx = int(1/dx)
     Nz = int(1/dz)
-    z_ax = np.linspace((zlim[0]-q)*Nz, (zlim[1]+q-1)*Nz,  (zlim[1]+2*q-zlim[0])*Nz)
-    x_ax = np.linspace((xlim[0]-q)*Nx, (xlim[1]+q-1)*Nx,  (xlim[1]+2*q-xlim[0])*Nx)
+    z_ax = np.linspace((zlim[0])*Nz, (zlim[1]-1)*Nz,  (zlim[1]-zlim[0])*Nz)
+    x_ax = np.linspace((xlim[0])*Nx, (xlim[1]-1)*Nx,  (xlim[1]-xlim[0])*Nx)
     x_ax, z_ax = np.meshgrid(x_ax,z_ax)
-    fig = plt.figure()
+    fig = plt.figure(figsize=(20,10))
     ax = plt.axes(projection='3d')
     surf = ax.plot_surface(z_ax, x_ax, rho.T,cmap='viridis')
     ax.set_xlabel('Z')
